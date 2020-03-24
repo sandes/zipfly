264219b739a240a4e27513cc79da14b7e1004c0b
sandes <santidebus@gmail.com>
2020-03-24 00:04:09 -0300

    readme

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# zipfly
+# Buzon - ZipFly
+
+ZipFly is a zip archive generator based on zipfile.py.
+It was created by Buzon.io to generate a zipfly on-the-fly for download in a python application.
+
+
+# Install
+    pip install zipfly
+
+# Usage
+
+```python
+    from zipfile import ZIP_DEFLATED
+    import zipfly
+
+    filelist = [
+        "/to/path/large-file1.mp4",
+        "/to/path/large-file2.mp4",
+    ]
+
+    z = zipfly.ZipFile( mode='w',
+                        compression=ZIP_DEFLATED, 
+                        allowZip64=True)
+    for f in filelist:
+        # get filename and write
+        filename = os.path.basename(os.path.normpath(f))
+        z.write(f, filename)
+```
+
+# Requirements
+Python > 2.7
+
+# License
+This library was created by Buzon.io and is released under the MIT. Copyright 2019 Grow HQ, Inc.
--- setup.py
+++ setup.py
@@ -1,1 +1,46 @@
- 
+from setuptools import setup
+import sys
+import os
+
+
+CURRENT_PYTHON = sys.version_info[:2]
+REQUIRED_PYTHON = (2,7)
+
+# This check and everything above must remain compatible with Python 2.7.
+if CURRENT_PYTHON < REQUIRED_PYTHON:
+    
+    sys.stderr.write("""
+        ==========================
+        Unsupported Python version
+        ==========================
+        This version of ZipStreaming requires Python {}.{}, but you're trying to
+        install it on Python {}.{}.
+        This may be because you are using a version of pip that doesn't
+        understand the python_requires classifier""".format(*(REQUIRED_PYTHON + CURRENT_PYTHON)))
+    sys.exit(1)
+
+setup(
+    name='zipfly',
+    packages=['zipfly'],
+    description='ZipFly',
+    version='1.0',
+    url='http://github.com/buzonIO/zipfly',
+    download_url = 'https://github.com/BuzonIO/zipfly/archive/v1.0.tar.gz',
+    author='Buzon',
+    author_email='support@buzon.io',
+    keywords=['zipfly','buzon'],
+    install_requires=[],    
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',  
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        
+    ],    
+) 
