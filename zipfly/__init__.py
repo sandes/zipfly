22d8f0515575b9c34bedc124ef4ade9142b324a6
sandes <santidebus@gmail.com>
2020-03-24 21:17:49 -0300

    version

--- zipfly/__init__.py
+++ zipfly/__init__.py
@@ -6,7 +6,7 @@ Derived directly from zipfile.py
 """
 from __future__ import unicode_literals, print_function, with_statement
 
-__version__ = '1.0.1'
+__version__ = '1.1'
 
 from zipfile import (
     ZIP_STORED,
