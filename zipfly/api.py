14bfc5b340e01dec98810f692a44db0e15555488
sandes <santidebus@gmail.com>
2020-04-12 09:36:41 -0300

    from one file

--- zipfly/api.py
+++ zipfly/api.py
@@ -3,20 +3,16 @@
 import sys
 
 
-def from_one_file(file_object, chunk_size=1024*16):
+def from_one_file(file_object):
     
     """Lazy function (generator) to read a file piece by piece.
     Default chunk size 1024 * 16 bytes"""
 
     while True:
-        data = file_object.read(chunk_size)
+        data = file_object.read(1024*16)
         if not data:
             break
         yield data
 
 
 
-
-    with open('really_big_file.dat') as f:
-        for piece in from_one_file(f):
-            process_data(piece)
