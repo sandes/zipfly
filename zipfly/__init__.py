75cbf16357b0c6cdef2efc25926a20ae893a9f3f
sandes <santidebus@gmail.com>
2020-04-12 09:50:21 -0300

    version

--- zipfly/__init__.py
+++ zipfly/__init__.py
@@ -7,7 +7,7 @@ Derived directly from zipfile.py
 from __future__ import unicode_literals, print_function, with_statement
 
 __version__ = '1.1.8'
-__author__ = 'Santiago Debus - Buzon.io'
+__author__ = 'Santiago Debus - Grow HQ, Inc.'
 __license__ = 'MIT'
 
 from .zipfly import ZipFly
