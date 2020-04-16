c7c265a52e9af03a36bb52206476a6b4112d786a
sandes <santidebus@gmail.com>
2020-04-16 09:34:33 -0300

    c

--- zipfly/api.py
+++ zipfly/api.py
@@ -5,7 +5,7 @@ import sys
 
 def from_one_file(file_location):
 
-    """Lazy function (generator) to read a file piece by piece.
+    """read a file piece by piece.
     Default chunk size 1024 * 16 bytes"""
 
     with open(file_location, 'rb') as entry:
