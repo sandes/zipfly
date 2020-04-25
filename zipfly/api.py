8fd2d27f6b64fa0363a517400eaccc3f2c1bfa32
sandes <santidebus@gmail.com>
2020-04-25 18:18:31 -0300

    new method to read bytes from string

--- zipfly/api.py
+++ zipfly/api.py
@@ -15,3 +15,17 @@ def from_one_file(file_location):
             yield chunk
 
 
+class Utils:
+
+    def string_size_in_bytes(filename):
+        
+        # encode to utf-8 and get string's size
+        """ python3 zipfile
+        """         
+
+        tmp_b = 0
+        for c in filename:
+            tmp_b += len( c.encode('utf-8') ) * int(0x2)
+        
+        # bytes size
+        return tmp_b 
