2b964e96b99678472a21396888922d19cad9748c
markflush <mark@buzon.io>
2020-05-05 12:46:49 -0300

    delete buffer on api

--- zipfly/api.py
+++ zipfly/api.py
@@ -22,8 +22,6 @@ class Buffer:
         self.pfbs = 0
         self.storesize = int( ss )
         self.ss = 0x2
-	      self.buffer = 0x2
-	      self.buffer_b = 0x8
 
 
     def paths_size_in_bytes(self):
