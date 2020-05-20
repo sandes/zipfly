7ed97e111a2a23c00fe1775bede3d719f81a79a6
sandes <santidebus@gmail.com>
2020-05-20 18:49:12 -0300

    add test script with prediction size

--- zipfly/test.py
+++ zipfly/test.py
@@ -1,2 +1,48 @@
 #v3.0.1
-#integration
+import unittest
+import zipfly
+import os
+
+
+paths = []
+
+for dirpath, dnames, fnames in os.walk("../dist/"):
+    for f in fnames:
+        paths.append({'fs':f'../dist/{f}'})
+
+for dirpath, dnames, fnames in os.walk("../zipfly.egg-info/"):
+    for f in fnames:
+        paths.append({'fs':f'../dist/{f}'})
+
+for path in paths:
+    path['n'] = path['fs']
+
+
+class TestBufferPredictionSize(unittest.TestCase):
+
+    def test_size(self):
+
+        storesize = 0
+        for path in paths:
+            f = open(path['fs'], 'rb')
+            storesize += os.fstat(f.fileno()).st_size
+
+        zfly = zipfly.ZipFly( paths = paths, storesize = storesize )
+
+        # zip size before creating it in bytes
+        ps = zfly.buffer_prediction_size()
+
+        with open("test.zip", "wb") as f:
+            for i in zfly.generator():
+                f.write(i)
+
+        f = open('test.zip', 'rb')
+        zs = os.fstat(f.fileno()).st_size
+
+        self.assertEqual(zs,ps)    
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+
