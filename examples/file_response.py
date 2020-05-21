69cf62a052349415131354154d7c6f48083261dc
sandes <santidebus@gmail.com>
2020-05-21 03:13:44 -0300

    FileResponse; streaming one file

--- examples/file_response.py
+++ examples/file_response.py
@@ -3,7 +3,8 @@ import zipfly
 file_location = '/home/user/Documents/file-100-GB.csv'
 
 # Efficient way to read a single very large binary file in python
-go_to_streaming = zipfly.from_one_file( file_location )
+go_to_streaming = zipfly.FileResponse( file_location )
 
 print ( go_to_streaming )
 # <generator object from_one_file at 0x7f85aad34a50>
+ 
