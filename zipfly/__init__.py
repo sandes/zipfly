8de4c807522c5e48ae5c3954de7ae19d8e80b5bd
sandes <santidebus@gmail.com>
2020-05-21 03:01:38 -0300

    remove buffer class

--- zipfly/__init__.py
+++ zipfly/__init__.py
@@ -2,9 +2,10 @@
 
 from __future__ import unicode_literals, print_function, with_statement
 
-__version__ = '3.0.6'
+__version__ = '4.0.1'
 __author__ = 'Santiago Debus - Grow HQ, Inc.'
 __license__ = 'MIT'
 
 from .zipfly import ZipFly
-from .api import from_one_file
+from .response import FileResponse
+from .api import Buffer
