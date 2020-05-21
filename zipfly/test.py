ac5e628c45ac372c61d383445f8965bff81cf1ea
sandes <santidebus@gmail.com>
2020-05-21 05:23:31 -0300

    unit testing -- buffer prediction size -- intagration zip

--- zipfly/test.py
+++ zipfly/test.py
@@ -2,40 +2,75 @@ import unittest
 import zipfly
 import os
 
+directory = os.getcwd()
+list1 = []
 
-paths = []
+p1 = os.getcwd()
+p2 = os.getcwd()
+p3 = os.getcwd()
 
-for dirpath, dnames, fnames in os.walk("../dist/"):
+
+idx1 = p1.rfind("/zipfly")
+p1 = p1[0:idx1]
+p1 = p1+"/dist/"
+
+idx2 = p2.rfind("/zipfly")
+p2 = p2[0:idx2]
+p2 = p2+"/zipfly.egg-info/"
+
+idx3 = p3.rfind("/zipfly")
+p3 = p3[0:idx3]
+p3 = p3+"/examples/"
+
+paths1 = []
+paths2 = []
+paths3 = []
+
+for dirpath, dnames, fnames in os.walk(p1):
     for f in fnames:
-        paths.append({'fs':'../dist/{}'.format(f)})
+        paths1.append(
+            {
+                'fs':'{}{}'.format(p1,f),
+                'n':'{}{}'.format(p1,f),
+            }
+        )
 
-for dirpath, dnames, fnames in os.walk("../zipfly.egg-info/"):
+for dirpath, dnames, fnames in os.walk(p2):
     for f in fnames:
-        paths.append({'fs':'../zipfly.egg-info/{}'.format(f)})
+        paths2.append(
+            {
+                'fs':'{}{}'.format(p2,f),
+                'n':'{}{}'.format(p2,f),
+            }
+        )
 
-for dirpath, dnames, fnames in os.walk("../examples/"):
+for dirpath, dnames, fnames in os.walk(p3):
     for f in fnames:
-        paths.append({'fs':'../examples/{}'.format(f)})        
+        paths3.append(
+            {
+                'fs':'{}{}'.format(p3,f),
+                'n':'{}{}'.format(p3,f),
+            }
+        )    
 
-for path in paths:
-    path['n'] = path['fs']
+
 
 
 class TestBufferPredictionSize(unittest.TestCase):
 
-    def test_size(self):
+    def test_paths1(self):
 
-        for test_n in range(1, 100):
+        for test_n in range(1, 5):
 
             with self.subTest(i=test_n):
             
                 storesize = 0
-                for path in paths:
+                for path in paths1:
                     f = open(path['fs'], 'rb')
                     storesize += os.fstat(f.fileno()).st_size
                     f.close()
 
-                zfly = zipfly.ZipFly( paths = paths, storesize = storesize )
+                zfly = zipfly.ZipFly( paths = paths1, storesize = storesize )
 
                 # zip size before creating it in bytes
                 ps = zfly.buffer_prediction_size()
@@ -52,7 +87,65 @@ class TestBufferPredictionSize(unittest.TestCase):
 
                 self.assertEqual(zs,ps)    
 
+    def test_paths2(self):
+
+        for test_n in range(1, 5):
+
+            with self.subTest(i=test_n):
+            
+                storesize = 0
+                for path in paths2:
+                    f = open(path['fs'], 'rb')
+                    storesize += os.fstat(f.fileno()).st_size
+                    f.close()
+
+                zfly = zipfly.ZipFly( paths = paths2, storesize = storesize )
+
+                # zip size before creating it in bytes
+                ps = zfly.buffer_prediction_size()
+
+                with open("test{}.zip".format(test_n), "wb") as f:
+                    for i in zfly.generator():
+                        f.write(i)
+
+                f = open("test{}.zip".format(test_n), 'rb')
+                zs = os.fstat(f.fileno()).st_size
+                f.close()
+
+                print ("FINAL SIZE vs PREDICTION:", zs, "--", ps, (" ---- OK" if zs==ps else "FAIL"))
+
+                self.assertEqual(zs,ps)    
+
+    def test_paths3(self):
+
+        for test_n in range(1, 5):
+
+            with self.subTest(i=test_n):
+            
+                storesize = 0
+                for path in paths3:
+                    f = open(path['fs'], 'rb')
+                    storesize += os.fstat(f.fileno()).st_size
+                    f.close()
+
+                zfly = zipfly.ZipFly( paths = paths3, storesize = storesize )
+
+                # zip size before creating it in bytes
+                ps = zfly.buffer_prediction_size()
+
+                with open("test{}.zip".format(test_n), "wb") as f:
+                    for i in zfly.generator():
+                        f.write(i)
+
+                f = open("test{}.zip".format(test_n), 'rb')
+                zs = os.fstat(f.fileno()).st_size
+                f.close()
+
+                print ("FINAL SIZE vs PREDICTION:", zs, "--", ps, (" ---- OK" if zs==ps else "FAIL"))
+
+                self.assertEqual(zs,ps)                    
 
 if __name__ == '__main__':
     
     unittest.main()
+
