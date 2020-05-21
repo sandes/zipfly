8507803703cc0dbfd63a3bbdb5180dd3d37f7b6d
sandes <santidebus@gmail.com>
2020-05-21 03:37:51 -0300

    unit testing; BufferPredictionSize

--- zipfly/test.py
+++ zipfly/test.py
@@ -3,6 +3,8 @@ import zipfly
 import os
 
 
+print ("Zipfly version:", zipfly.__version__)
+
 paths = []
 
 for dirpath, dnames, fnames in os.walk("../dist/"):
@@ -21,25 +23,27 @@ class TestBufferPredictionSize(unittest.TestCase):
 
     def test_size(self):
 
-        storesize = 0
-        for path in paths:
-            f = open(path['fs'], 'rb')
-            storesize += os.fstat(f.fileno()).st_size
+        for _ in range(10):
+            storesize = 0
+            for path in paths:
+                f = open(path['fs'], 'rb')
+                storesize += os.fstat(f.fileno()).st_size
+                f.close()
+
+            zfly = zipfly.ZipFly( paths = paths, storesize = storesize )
+
+            # zip size before creating it in bytes
+            ps = zfly.buffer_prediction_size()
+
+            with open("test.zip", "wb") as f:
+                for i in zfly.generator():
+                    f.write(i)
+
+            f = open('test.zip', 'rb')
+            zs = os.fstat(f.fileno()).st_size
             f.close()
 
-        zfly = zipfly.ZipFly( paths = paths, storesize = storesize )
-
-        # zip size before creating it in bytes
-        ps = zfly.buffer_prediction_size()
-
-        with open("test.zip", "wb") as f:
-            for i in zfly.generator():
-                f.write(i)
-
-        f = open('test.zip', 'rb')
-        zs = os.fstat(f.fileno()).st_size
-
-        self.assertEqual(zs,ps)    
+            self.assertEqual(zs,ps)    
 
 
 
