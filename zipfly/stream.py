f2491e6c0d7944a578c0c4f19877d1f6701b58c4
sandes <santidebus@gmail.com>
2020-05-25 01:16:10 -0300

    Stream class was added to ZipFly

---
+++ zipfly/stream.py
@@ -0,0 +1,31 @@
+import io
+
+class ZipflyStream( io.RawIOBase ):
+
+    """
+    The RawIOBase ABC extends IOBase. It deals with
+    the reading and writing of bytes to a stream. FileIO subclasses
+    RawIOBase to provide an interface to files in the machine’s file system.
+    """
+
+    def __init__(self):
+        self._buffer = b''
+        self._size = 0
+
+    def writable(self):
+        return True
+
+    def write(self, b):
+        if self.closed:
+            raise RuntimeError("ZipFly stream was closed!")
+        self._buffer += b
+        return len(b)
+
+    def get(self):
+        chunk = self._buffer
+        self._buffer = b''
+        self._size += len(chunk)
+        return chunk
+
+    def size(self):
+        return self._size
