691cc31d203f9516313a7d3e566c7f8c3a28e9bf
sandes <santidebus@gmail.com>
2020-05-28 05:54:21 -0300

    Dump version; master is now 6.0.3

--- zipfly/__init__.py
+++ zipfly/__init__.py
@@ -2,10 +2,11 @@
 
 from __future__ import unicode_literals, print_function, with_statement
 
-__version__ = '6.0.2'
+__version__ = '6.0.3'
 __author__ = 'Santiago Debus - Grow HQ, Inc.'
 __license__ = 'MIT'
 
 from .zipfly import ZipFly
+from .zipfly import LargePredictionSize
 from .response import FileResponse
 from .api import Buffer
