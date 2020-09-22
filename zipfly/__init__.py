2771e6f73cc8e12722179dfe7609c8a923455b2a
sandes <santidebus@gmail.com>
2020-09-22 18:27:02 -0300

    init--

--- zipfly/__init__.py
+++ zipfly/__init__.py
@@ -3,7 +3,7 @@
 from __future__ import unicode_literals, print_function, with_statement
 
 __version__ = '6.0.3'
-__author__ = 'Santiago Debus - Grow HQ, Inc.'
+__author__ = 'Santiago Debus - Buzon, Inc.'
 __license__ = 'MIT'
 
 from .zipfly import ZipFly
