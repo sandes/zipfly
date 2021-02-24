088975f294b551ec9511652ca0c1195f44c0232f
sandes <santidebus@gmail.com>
2021-02-24 20:00:41 -0300

    constructor

--- zipfly/__init__.py
+++ zipfly/__init__.py
@@ -5,6 +5,7 @@ from __future__ import unicode_literals, print_function, with_statement
 __version__ = '6.0.4'
 __author__ = 'Santiago Debus - Buzon, Inc.'
 __license__ = 'MIT'
+#
 
 from .zipfly import ZipFly
 from .zipfly import LargePredictionSize
