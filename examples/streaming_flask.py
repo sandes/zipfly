a491cf0f92b31f83eee95087276e5549c2c35684
sandes <santidebus@gmail.com>
2022-06-25 19:05:31 -0300

    flask streaming

--- examples/streaming_flask.py
+++ examples/streaming_flask.py
@@ -8,33 +8,17 @@ paths = [
         'fs': 'home/user/Videos/jupiter.mp4',
         'n': 'movies/jupiter.mp4',
     },
-    {
-        'fs': 'home/user/Documents/mercury.mp4',
-        'n': 'movies/mercury.mp4',
-    },
 ]
 
 
-storesize = 92896201 # (jupiter.mp4 + mercury.mp4) size in bytes
-
-
 # constructor
-zfly = zipfly.ZipFly( mode='w', paths=paths, storesize=storesize )
+zfly = zipfly.ZipFly( paths=paths )
 
 # z is a new generator
-# (<generator object generator at 0x7f85aad34a50>)
 z = zfly.generator()
 
 # flask streaming
-response = Response(
-    z,
-    mimetype='application/zip',
-)
-
-# zip size before creating it
-content_length = zfly.buffer_prediction_size()
-
-response.headers['Content-Length'] = content_length
+response = Response(z, mimetype='application/zip',)
 response.headers['Content-Disposition'] = 'attachment; filename=file.zip'
 
 return response
