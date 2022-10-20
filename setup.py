d36767bb78b46c2bb23eda4d4183a9961cfdb36c
sandes <santidebus@gmail.com>
2022-10-20 02:33:36 -0300

    python3.10

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 import sys
 import os
-__version__ = '6.0.4'
+__version__ = '6.0.5'
 
 CURRENT_PYTHON = sys.version_info[:2]
 REQUIRED_PYTHON = (3,6)
@@ -26,13 +26,13 @@ setup(
     packages=['zipfly'],
     description='ZipFly',
     version=__version__,
-    url='http://github.com/buzonIO/zipfly',
-    download_url = f'https://github.com/BuzonIO/zipfly/archive/v{__version__}.tar.gz',
-    author='Buzon',
+    url='http://github.com/sandes/zipfly',
+    download_url = f'https://github.com/sandes/zipfly/archive/v{__version__}.tar.gz',
+    author='sandes',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    author_email='support@buzon.io',
-    keywords=['zipfly','buzon'],
+    author_email='santidebus@gmail.com',
+    keywords=['zipfly','Santiago Debus','santiagodebus.com'],
     install_requires=[],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
@@ -43,5 +43,6 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
 )
